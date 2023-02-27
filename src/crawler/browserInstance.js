a8047ad29734513140a940a96563f66df4d62a92
wclark8 <wclark8>
2023-02-27 15:35:15 +0000

    moving env vars to process

--- src/crawler/browserInstance.js
+++ src/crawler/browserInstance.js
@@ -6,18 +6,21 @@ async function browserInstanceLaunch() {
 	puppeteer.use(StealthPlugin())
     let browser;
 	try {
-	    console.log("Opening the browser......");
-	    /*browser = await puppeteer.launch({
-	        headless: true,
-			executablePath: executablePath(),
-	        args: ["--disable-setuid-sandbox", "--no-sandbox"],
-	        'ignoreHTTPSErrors': true,
-			defaultViewport: null,
-	    }); */
+		console.log("Opening the browser......");
+		if (process.env.NODE_ENV === 'local') {
+			browser = await puppeteer.launch({
+				headless: true,
+				executablePath: executablePath(),
+				args: ["--disable-setuid-sandbox", "--no-sandbox"],
+				'ignoreHTTPSErrors': true,
+				defaultViewport: null,
+			});
+		} else {
+			browser = await puppeteer.connect({
+			   browserWSEndpoint: 'wss://chrome.browserless.io?token=' + process.env.BROWSERLESS_TOKEN
+			 });
+		}
 
-		 browser = await puppeteer.connect({
-			browserWSEndpoint: 'wss://chrome.browserless.io?token=3f40c62a-205a-4655-8687-a70e559892df'
-		  });
 	} catch (err) {
 	    console.log("Could not create a browser instance => : ", err);
 	}
